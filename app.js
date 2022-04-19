4b8b6065911ac637d0d69ab23806c921be641f58
Mireille <miramimbembe1991@gmail.com>
2022-04-19 14:49:03 +0530

    create passport wit cooked

--- app.js
+++ app.js
@@ -2,27 +2,48 @@ require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const session = require('express-session');
+const passport = require("passport");
+const passpoortlocalmongoose = require("passport-local-mongoose");
 
-const salRounds=10;
 const app = express();
-
-
-
 console.log(process.env.API_KEY);
-
-
-
 app.use(bodyParser.urlencoded({extended: true }));
+
+app.use(session({
+    secret: "MAMAN NA BISO",
+    resave: true,
+    saveUninitialized: true,
+
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
+
 const userSchema = new mongoose.Schema( {
     email:String,
     password:String,
 });
+userSchema.plugin(passpoortlocalmongoose);
+
 
 const User = new mongoose.model("User",userSchema);
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+
+
+
+
+
+
 
 
 
@@ -43,39 +64,12 @@ app.get("/register" ,function(req,res){
 
 });
 app.post("/register",function(req, res){
-    bcrypt.hash(req.body.password,salRounds ,function(err,hash){
+    
 
-const user1=req.body.username;
-const user2 =hash;
-const newUser = new User({email:user1, password:user2});
-newUser.save(function(err){
-        if(!err){
-            res.render("secrets");
-        }
 
-});
-
-});
 });
 app.post("/login",function(req,res){
-    const username = req.body.username;
-    const pass = req.body.password;
-    User.findOne({email:username},function(err,founderUSER){
-        if(err){
-            console.log(err);
-        }else{
-            if(founderUSER){
-                bcrypt.compare(pass,founderUSER.user2 ,function(err,resulta){
-            if(resulta === true){
-            }
-                
-                
-});
-}
-}         
-});      
-res.render("secrets");
- 
+    
 });
 
 
