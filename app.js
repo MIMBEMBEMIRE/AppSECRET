417a0856d3963a9018c2b9d0ce5f4efe00104751
Mireille <miramimbembe1991@gmail.com>
2022-04-19 08:05:24 +0530

    gitignore

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
-
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const res = require("express/lib/response");
 const encrypt = require("mongoose-encryption");
 const app = express();
+console.log(process.env.API_KEY);
 
 
 app.use(bodyParser.urlencoded({extended: true }));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
-const userSchema = new mongoose.Schema( {
+const userSchema = mongoose.Schema( {
     email:String,
     password:String,
 });
 
-const secret ="thisiourlittelesecret";
-userSchema.plugin(encrypt,{secret:secret,EncryptedField:["password"]});
-
-
-
-
-const User=new mongoose.model("User",userSchema);
+userSchema.plugin(encrypt,{secret:process.env.SECRET,EncryptedField:["password"]});const User= new mongoose.model("User",userSchema);
 
 app.get("/" ,function(req,res){
     res.render("home")
